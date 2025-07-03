efc106dbc860cb74e384c8786c63f68159913b34
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2025-07-03 11:43:32 -0400

    Adding support for multiple modules for handling local, global, and const potentially

--- libModularParser/src/main/java/redactedrice/modularparser/basic/BasicVariableModule.java
+++ libModularParser/src/main/java/redactedrice/modularparser/basic/BasicVariableModule.java
@@ -4,22 +4,58 @@ package redactedrice.modularparser.basic;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import redactedrice.modularparser.LineHandler;
 import redactedrice.modularparser.VariableHandler;
+import redactedrice.modularparser.WordReserver;
 
-public class BasicVariableModule extends LineStartMatchModule implements VariableHandler {
-    private final static Pattern varDef = Pattern.compile("^\\s*variable\\s+(\\w+)\\s*=\\s*(.+)$");
+public class BasicVariableModule extends BaseModule
+        implements LineHandler, VariableHandler, WordReserver {
+    public enum ImplicitType {
+        NONE, KEYWORD, ALL
+    }
 
-    private final Map<String, Object> variables = new LinkedHashMap<>();
+    protected final String key;
+    protected final Map<String, ReservedType> reservedWords = new HashMap<>();
+    protected final Map<String, Object> variables = new HashMap<>();
+    protected final Pattern varDef;
 
-    protected BasicVariableModule() {
-        super("BasicVariableHandler", "variable");
+    public BasicVariableModule(String moduleName, String shareableKeyword,
+            String shareableQualifier, ImplicitType implicitType) {
+        super(moduleName);
+        key = shareableQualifier + " " + shareableKeyword;
+        reservedWords.put(key, ReservedType.EXCLUSIVE);
+        reservedWords.put(shareableKeyword, ReservedType.SHAREABLE);
+        reservedWords.put(shareableQualifier, ReservedType.SHAREABLE);
+
+        switch (implicitType) {
+        case ALL:
+            varDef = Pattern.compile("^\\s*(?:" + shareableQualifier + "\\s+)?(?:"
+                    + shareableKeyword + "\\s+)?(\\w+)\\s*=\\s*(.+)$");
+            break;
+        case KEYWORD:
+            varDef = Pattern.compile("^\\s*" + shareableQualifier + "\\s+(?:" + shareableKeyword
+                    + "\\s+)?(\\w+)\\s*=\\s*(.+)$");
+            break;
+        default: // NONE
+            varDef = Pattern.compile("^\\s*" + shareableQualifier + "\\s+" + shareableKeyword
+                    + "\\s+(\\w+)\\s*=\\s*(.+)$");
+            break;
+        }
+
+    }
+
+    @Override
+    public boolean matches(String logicalLine) {
+        if (logicalLine == null || logicalLine.isBlank()) {
+            return false;
+        }
+        return varDef.matcher(logicalLine).matches();
     }
 
     @Override
@@ -39,7 +75,8 @@ public class BasicVariableModule extends LineStartMatchModule implements Variabl
         Object obj = parser.evaluateLiteral(literal);
         if (obj != null) {
             variables.put(varName, obj);
-            System.out.println("Variable: Added variable " + varName + " with value: " + obj);
+            System.out
+                    .println(getName() + ": Added " + key + " " + varName + " with value: " + obj);
         } else {
             throw new IllegalArgumentException("VariableHandler: For variable \"" + varName
                     + "\" + cannot parse value: " + literal);
@@ -48,25 +85,26 @@ public class BasicVariableModule extends LineStartMatchModule implements Variabl
 
     @Override
     public boolean isReservedWord(String word, Optional<ReservedType> type) {
-        if (super.isReservedWord(word, type)) {
-            return true;
+        if (type.isEmpty()) {
+            return reservedWords.containsKey(word) || isVariable(word);
+        } else if (type.get() == ReservedType.SHAREABLE) {
+            return reservedWords.get(word) == ReservedType.SHAREABLE || isVariable(word);
+        } else {
+            return reservedWords.get(word) == type.get();
         }
-        if (type.isEmpty() || type.get() == ReservedType.SHAREABLE) {
-            return isVariable(word);
-        }
-        return false;
     }
 
     @Override
     public Map<String, ReservedType> getAllReservedWords() {
-        Map<String, ReservedType> all = new HashMap<>(super.getAllReservedWords());
+        Map<String, ReservedType> all = new HashMap<>(reservedWords);
         getVariables().keySet().stream().forEach(alias -> all.put(alias, ReservedType.SHAREABLE));
         return Collections.unmodifiableMap(all);
     }
 
     @Override
     public Set<String> getReservedWords(ReservedType type) {
-        Set<String> all = new HashSet<>(super.getReservedWords(type));
+        Set<String> all = new HashSet<>(reservedWords.entrySet().stream()
+                .filter(entry -> entry.getValue() == type).map(Map.Entry::getKey).toList());
         all.addAll(variables.keySet());
         return Collections.unmodifiableSet(all);
     }
