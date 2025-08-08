543dc257b13cc93857b8f06ab1e29d1d98a6dbfd
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2025-08-08 17:42:45 -0400

    Adding tests for supporting classes and starting default scope supporter tests

---
+++ libModularParser/src/test/java/redactedrice/modularparser/scope/BaseScopedParserTests.java
@@ -0,0 +1,52 @@
+package redactedrice.modularparser.scope;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import redactedrice.modularparser.core.ModularParser;
+
+class BaseScopedParserTests {
+    final String NAME = "ScopedParser";
+
+    private BaseScopedParser testee;
+    private ModularParser parser;
+    private ScopeSupporter scopeSupporter;
+
+    private class BaseScopedParserTester extends BaseScopedParser {
+        protected BaseScopedParserTester(String name) {
+            super(name);
+        }
+
+        @Override
+        public boolean tryParseScoped(String scope, String logicalLine, String defaultScope) {
+            return false;
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        testee = spy(new BaseScopedParserTester(NAME));
+        parser = mock(ModularParser.class);
+        scopeSupporter = mock(ScopeSupporter.class);
+
+        when(parser.getSupporterOfType(ScopeSupporter.class)).thenReturn(scopeSupporter);
+        testee.setParser(parser);
+    }
+
+    @Test
+    void constructorSetterTest() {
+        assertEquals(NAME, testee.getName());
+    }
+
+    @Test
+    void setModuleRefsTest() {
+        testee.setModuleRefs();
+        assertEquals(scopeSupporter, testee.scopeSupporter);
+    }
+}
