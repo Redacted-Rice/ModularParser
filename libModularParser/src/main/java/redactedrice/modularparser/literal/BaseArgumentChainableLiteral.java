ddc2660fc8117d01c7daab9503e8c6b9ee02eb76
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2025-09-17 21:13:43 -0400

    Added positional offset to account for chained arg

--- libModularParser/src/main/java/redactedrice/modularparser/literal/BaseArgumentChainableLiteral.java
+++ libModularParser/src/main/java/redactedrice/modularparser/literal/BaseArgumentChainableLiteral.java
@@ -2,9 +2,11 @@ package redactedrice.modularparser.literal;
 
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import redactedrice.modularparser.core.Response;
+import redactedrice.modularparser.core.LogSupporter.LogLevel;
 import redactedrice.modularparser.lineformer.Grouper;
 
 public abstract class BaseArgumentChainableLiteral extends BaseArgumentLiteral
@@ -35,6 +37,39 @@ public abstract class BaseArgumentChainableLiteral extends BaseArgumentLiteral
             return Response.notHandled();
         }
         return tryEvaluateObject(parsedArgs);
+    }
+
+    @Override
+    protected boolean handlePositionalArgs(List<String> positionalParams,
+            Map<String, Object> parsedArgs) {
+        int requiredIdx = 0;
+        int optionalIdx = 0;
+        while (requiredIdx + optionalIdx < positionalParams.size()) {
+            int combined = requiredIdx + optionalIdx;
+            String literal = positionalParams.get(combined);
+            
+            String argName;
+            if (requiredIdx < requiredArgs.length) {
+            	argName = requiredArgs[requiredIdx++];
+            } else if (optionalIdx < optionalArgs.length) {
+            	argName = optionalArgs[optionalIdx++];
+            } else {
+                log(LogLevel.ERROR, "Too many args were found: %s", positionalParams.toString());
+                return false;
+            } 
+            if (argName.equals(chainedArg) && parsedArgs.containsKey(argName)) {
+            	// Already added to the args
+            	continue;
+            }
+            
+            Response<Object> parsed = literalSupporter.evaluateLiteral(literal);
+            if (!parsed.wasValueReturned()) {
+                log(LogLevel.ERROR, "Failed to parse arg %s at index %d", literal, combined);
+                return false;
+            }
+            parsedArgs.put(argName, parsed.getValue());
+        }
+        return true;
     }
 
     public abstract Response<Object> tryEvaluateObject(Map<String, Object> args);
