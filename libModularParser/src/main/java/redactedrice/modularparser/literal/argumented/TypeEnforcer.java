0bbc78433416c9ab45d0d606ef27465b80294e08
DasKeifer <43280661+DasKeifer@users.noreply.github.com>
2025-09-29 10:41:59 -0400

    Minor refactoring and adding a string enum arg parser

--- libModularParser/src/main/java/redactedrice/modularparser/literal/argumented/TypeEnforcer.java
+++ libModularParser/src/main/java/redactedrice/modularparser/literal/argumented/TypeEnforcer.java
@@ -3,19 +3,26 @@ package redactedrice.modularparser.literal.argumented;
 
 import redactedrice.modularparser.core.Response;
 
-public class TypeEnforcer<T> extends ArgumentParserBase<T> {
+public class TypeEnforcer<T> extends ArgumentParserBase {
+    protected final Class<T> clazz;
 
     public TypeEnforcer(Class<T> clazz, boolean allowNull) {
-    	super(clazz, allowNull);
-    }
-    
-    public TypeEnforcer(Class<T> clazz) {
-    	this(clazz, false);
+        super(allowNull);
+        this.clazz = clazz;
     }
 
-	@Override
-	public Response<Object> tryParseNonNullArgument(Response<Object> parsed, String argument) {
-		// Nothing else to do here
+    public TypeEnforcer(Class<T> clazz) {
+        this(clazz, false);
+    }
+
+    @Override
+    public Response<Object> tryParseNonNullArgument(Response<Object> parsed, String argument) {
+        // Nothing else to do here
         return Response.error("Expected value of type " + clazz.getSimpleName());
-	}
+    }
+
+    @Override
+    protected Class<?> expectedType() {
+        return clazz;
+    }
 }
--- libModularParser/src/test/java/redactedrice/modularparser/testsupport/SimpleObjectArgumentParser.java
+++ libModularParser/src/test/java/redactedrice/modularparser/testsupport/SimpleObjectArgumentParser.java
@@ -4,14 +4,19 @@ package redactedrice.modularparser.testsupport;
 import redactedrice.modularparser.core.Response;
 import redactedrice.modularparser.literal.argumented.ArgumentParserBase;
 
-public class SimpleObjectArgumentParser extends ArgumentParserBase<SimpleObject> {
+public class SimpleObjectArgumentParser extends ArgumentParserBase {
 
     public SimpleObjectArgumentParser() {
-    	super(SimpleObject.class, true);
+        super(true);
     }
 
-	@Override
-	public Response<Object> tryParseNonNullArgument(Response<Object> parsed, String argument) {
-		return Response.is(new SimpleObject(42, false, argument, null));
-	}
+    @Override
+    public Response<Object> tryParseNonNullArgument(Response<Object> parsed, String argument) {
+        return Response.is(new SimpleObject(42, false, argument, null));
+    }
+
+    @Override
+    protected Class<?> expectedType() {
+        return SimpleObject.class;
+    }
 }
