874b5f938326f716d1b6b469875d6adb4581775e
Das Keifer <43280661+DasKeifer@users.noreply.github.com>
2025-10-05 23:46:07 -0400

    Adding stream and map type parsers

---
+++ libModularParser/src/main/java/redactedrice/modularparser/literal/argumented/ArgParserStream.java
@@ -0,0 +1,30 @@
+package redactedrice.modularparser.literal.argumented;
+
+
+import java.util.stream.Stream;
+
+import redactedrice.modularparser.core.Response;
+import redactedrice.reflectionhelpers.utils.ConversionUtils;
+
+public class ArgParserStream extends ArgumentParserBase {
+
+    public ArgParserStream(boolean allowNull) {
+        super(allowNull);
+    }
+
+    @Override
+    protected Response<Object> tryParseNonNullArgument(Response<Object> parsed, String argument) {
+    	if (parsed.wasHandled()) {
+        	Stream<Object> stream = ConversionUtils.convertToStreamOrNull(parsed.getValue());
+        	if (stream != null) {
+        		return Response.is(stream);
+        	}
+    	}
+        return Response.notHandled();
+    }
+
+    @Override
+    protected Class<?> expectedType() {
+        return Stream.class;
+    }
+}
--- libModularParser/src/main/java/redactedrice/modularparser/literal/argumented/ArgParserValueTypedMapBase.java
+++ libModularParser/src/main/java/redactedrice/modularparser/literal/argumented/ArgParserValueTypedMapBase.java
@@ -1,20 +1,19 @@
 package redactedrice.modularparser.literal.argumented;
 
 
+import java.util.Map;
+import java.util.stream.Stream;
+
 import redactedrice.modularparser.core.Response;
 
-public abstract class ArgumentParserBase implements ArgumentParser {
+public abstract class ArgParserValueTypedMapBase implements ArgumentParser {
     protected final boolean allowNull;
 
-    protected ArgumentParserBase(boolean allowNull) {
+    public ArgParserValueTypedMapBase(boolean allowNull) {
         this.allowNull = allowNull;
     }
 
     public Response<Object> tryParseArgument(Response<Object> parsed, String argument) {
-        if (parsed.wasValueReturned() && expectedType().isInstance(parsed.getValue())) {
-            return parsed;
-        }
-
         if ((parsed.wasHandled() && parsed.getValue() == null) || argument.isBlank() ||
                 argument.equalsIgnoreCase("null")) {
             if (allowNull) {
@@ -23,15 +22,16 @@ public abstract class ArgumentParserBase implements ArgumentParser {
                 return Response.error("Null value was passed but is not allowed");
             }
         }
-        Response<Object> response = tryParseNonNullArgument(parsed, argument);
-        if (response.wasNotHandled()) {
-        	return Response.error("Passed arguement was of the wrong type");
+        
+        if (parsed.wasValueReturned() && parsed.getValue() instanceof Map<?,?> map) {
+            if (!map.values().stream()
+            	    .allMatch(v -> v instanceof Stream<?>)) {
+            	return Response.error("Was not a map of stream");
+            }
+            return parsed;
         }
-        return response;
+        return Response.notHandled();
     }
-
-    protected abstract Response<Object> tryParseNonNullArgument(Response<Object> parsed,
-            String argument);
-
+    
     protected abstract Class<?> expectedType();
 }
